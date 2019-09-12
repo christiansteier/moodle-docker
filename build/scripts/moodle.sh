95764325937e639558da4abdbe3bdf2b8a89a9e8
Christian Steier <christiansteier@users.noreply.github.com>
2019-09-12 14:46:18 +0200

    Update moodle.sh

--- build/scripts/moodle.sh
+++ build/scripts/moodle.sh
@@ -1,8 +1,8 @@
 
 echo "\n[i] Install Moodle\n"
 cd /tmp
-curl -LO https://download.moodle.org/download.php/stable${VERSION}${RELEASE}/moodle-${VERSION}.${RELEASE}.9.tgz
-curl -LO https://download.moodle.org/stable${VERSION}${RELEASE}/moodle-${VERSION}.${RELEASE}.9.tgz.tgz.sha256
+curl -LO https://download.moodle.org/stable${VERSION}${RELEASE}/moodle-${VERSION}.${RELEASE}.9.tgz
+curl -LO https://download.moodle.org/stable${VERSION}${RELEASE}/moodle-${VERSION}.${RELEASE}.9.tgz.sha256
 echo "[i] Verifying integrity..."
 CHECKSUM_STATE=$(echo -n $(sha256sum -c moodle-${VERSION}.${RELEASE}.9.tgz.sha256) | tail -c 2)
 if [ "${CHECKSUM_STATE}" != "OK" ]; then echo "[!!] Checksum does not match!" && exit 1; fi
